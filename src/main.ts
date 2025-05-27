c4e188f8dd3dd5d9f7eb224b7cc4dbd50592bdab
Lionnel Mazuba Itetshi <85796554+maz-git-design@users.noreply.github.com>
2025-05-27 18:09:10 +0100

    ipv4 priority

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// 👇 Force IPv4 DNS resolution
+(require('dns') as any).setDefaultResultOrder('ipv4first');
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3002);
